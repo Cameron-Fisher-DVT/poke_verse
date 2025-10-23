434c3e75f553b18d4d97c803a6a57bc6f86cdd1b
Cameron-Fisher-DVT <cfisher@jhb.dvt.co.za>
2025-10-23 19:07:18 +0200

    [Feature][01] Add Home Screen For Pokemon

--- app/src/main/java/za/co/dvt/pokeverse/presentation/MainActivity.kt
+++ app/src/main/java/za/co/dvt/pokeverse/presentation/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import za.co.dvt.pokeverse.features.pokedex.presentation.PokedexScreen
 import za.co.dvt.pokeverse.presentation.ui.theme.PokeVerseTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             PokeVerseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    PokedexScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PokeVerseTheme {
-        Greeting("Android")
     }
 }
